afb953c01523b6afcab322fc4f6d5084c385db40
zhman1981 <zhman@ukr.net>
2022-08-21 13:17:09 +0300

    ready

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,36 @@
+import Notiflix from 'notiflix';
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return promise = new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      resolve({ position, delay });
+    } else {
+      reject({ position, delay });
+    }
+  });
+}
+
+const submitForm = document.querySelector('form');
+
+const onSubmitForm = (evt) => {
+  evt.preventDefault(); 
+  const promises = [];
+  let delayi = Number(submitForm.delay.value);
+  for (let i = 0; i < submitForm.amount.value; i++) {
+    createPromise(i, delayi)
+    .then(({ position, delay }) => {
+      setTimeout(() => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position+1} in ${delay}ms`);
+      }, delay);
+    })
+    .catch(({ position, delay }) => {
+      setTimeout(() => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position+1} in ${delay}ms`);
+      }, delay);
+    });
+    delayi += Number(submitForm.step.value);
   }
 }
+
+submitForm.addEventListener('submit', onSubmitForm);
